1948820fe9e828f44d83a8f896c321eceb15ea58
nkoort <maks-g@i.ua>
2022-01-14 13:35:55 +0200

    master

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-$(document).ready(function() {
-	$('.header__burger').click(function(event) {
-		$('.header__burger, .header__link').toggleClass('active');
+// $(document).ready(function() {
+// 	$('.header__burger').click(function(event) {
+// 		$('.header__burger, .header__link').toggleClass('active');
+// 	});
+// });
+
+
+
+// $(document).ready(function() {
+// 	$('.body__block1-item').click(function(event) {
+// 		$('.body__block1-item, .body__block1-item-price')
+// 		$('.body__block1-item, .body__block1-item-price').toggleClass('active');
+// 	});
+// });
+
+$(document).ready(function () {
+	$(".body__block1-item").click(function () {
+		$(".body__block1-item").removeClass('active');
+		$(".body__block1-item-price").removeClass('active');
+		$(this).toggleClass('active');
+		// $('.body__block1-item-price').toggleClass('active');
 	});
 });
